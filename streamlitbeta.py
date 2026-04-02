bbfb45a28f03948fbc4e7394691ec79e7039cea8
WTAnalysis <wt_analysis@outlook.com>
2026-04-02 20:08:45 +0100

    Update streamlitbeta.py

--- streamlitbeta.py
+++ streamlitbeta.py
@@ -58,9 +58,7 @@ if matchlink:
         import json
         import re
         import matplotlib.pyplot as plt
-        import warnings
-        from pandas.errors import SettingWithCopyWarning
-        warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
         import pandas as pd
         import numpy as np 
         from matplotlib.colors import to_rgba
